1a46723bdf413c84f7b1fe97766beeb0127bc6f9
Darren Watson <v_tec@live.com>
2021-07-14 21:24:21 -0500

    added a player

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,11 +5,13 @@ import { Route, IndexRoute } from 'react-router';
 import Header from './componets/Header';
 import Login from './componets/LoginForm';
 import Registration from './componets/RegistrationForm';
+import Player from './componet/Player'
 
 // all routes go here
 export default (
-    <Route path="/" componet={Header}>
-        <Route path="/login" componet={Login}/>
-        <Route path="/registrarion" componet={Registration}/>
+    <Route path="/" componet={ Header }>
+        <Route path="/login" componet={ Login }/>
+        <Route path="/registrarion" componet={ Registration }/>
+        <Route path="/player" componet={ Player }/>
     </Route>
 );
